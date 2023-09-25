cf34c90391361ef9dd12a1a4e68f27ce6c43b9b6
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-09-25 22:34:24 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+початок = int(input("Введіть початкове число діапазону: "))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+кінець = int(input("Введіть кінцеве число діапазону: "))
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+число = початок
+while число <= кінець:
+    if число % 7 == 0:
+        print(число)
+    число += 1
